a5ac12b74c109074204872f042266bba38557e1f
Eugene Yamenko <yamenko@me.com>
2019-05-24 20:42:47 +1000

    Add rounded icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/profile-pic.jpg`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
