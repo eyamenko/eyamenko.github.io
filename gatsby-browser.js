6de132253b2b77c2e1a3986a7abc63c17f817b5c
Eugene Yamenko <yamenko@me.com>
2019-09-29 21:37:17 +1000

    Include css for PrismJS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,9 @@
 // custom typefaces
 import 'typeface-montserrat';
 import 'typeface-merriweather';
+
 // Font Awesome
 import '@fortawesome/fontawesome-svg-core/styles.css';
+
+// PrismJS
+import 'prismjs/themes/prism-solarizedlight.css';
