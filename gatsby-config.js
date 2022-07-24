57049cea051a68d3fa4ab95845a94b249e7fc7ad
Eugene Yamenko <yamenko@me.com>
2022-07-24 23:24:54 +1000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static`,
+        name: `static`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
